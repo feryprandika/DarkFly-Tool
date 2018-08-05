f1dbfda0c12cbce5b13075021ee9e211f71a7568
Ms.ambari <41313785+Ranginang67@users.noreply.github.com>
2018-08-06 03:04:00 +0700

    Add files via upload

---
+++ install.py
@@ -0,0 +1,14 @@
+#!/usr/bin/jembod
+
+import os, sys
+
+bin = '/data/data/com.termux/files/usr/bin'
+
+print 'installing...'
+os.system('apt-get install hydra -y && apt-get install python2')
+os.system('clear && pt-get install php -y')
+os.system('mv .module/.hydra $cd %s && mv .module/.spm $cd %s' % (bin, bin))
+os.system('mv .module/readme.txt $cd %s' % (bin))
+os.system('cp lib %s' % (bin))
+os.system('python2 .module/.MN')
+os.system('python2 .module/.BR')
